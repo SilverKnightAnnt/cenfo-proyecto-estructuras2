b9b8234fba6d4c55a71059e901bc31855a9881d8
Ángel Marín <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-12-03 20:47:38 -0600

    This is a test kappa

---
+++ DataStructuresII_Project/Test.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Test.cpp
+ * Author: User
+ * 
+ * Created on December 3, 2018, 8:38 PM
+ */
+
+#include "Test.h"
+
+Test::Test() {
+}
+
+Test::Test(const Test& orig) {
+}
+
+Test::~Test() {
+}
+
---
+++ DataStructuresII_Project/Test.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Test.h
+ * Author: User
+ *
+ * Created on December 3, 2018, 8:38 PM
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+class Test {
+public:
+    Test();
+    Test(const Test& orig);
+    virtual ~Test();
+private:
+
+};
+
+#endif /* TEST_H */
+
